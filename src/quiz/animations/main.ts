69ae12631a72ad31946ddea06e1888d436db254e
Henry Rothenberg <hsr205@aol.com>
2025-02-12 09:29:29 -0500

    Complete requirement

--- src/quiz/animations/main.ts
+++ src/quiz/animations/main.ts
@@ -1,36 +1,64 @@
 const aliceTumbling1: Keyframe[] = [
-  { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(0)' }
+    {transform: 'rotate(0) scale(1)'},
+    {transform: 'rotate(360deg) scale(0)'}
 ];
 
 const aliceTiming1: KeyframeEffectOptions = {
-  duration: 2000,
-  iterations: 1,
-  fill: 'forwards'
+    duration: 2000,
+    iterations: 1,
+    fill: 'forwards'
 };
 
 const alice10 = document.querySelector<HTMLElement>("#alice1");
 const alice20 = document.querySelector<HTMLElement>("#alice2");
 const alice30 = document.querySelector<HTMLElement>("#alice3");
 
-if(alice10 && alice20 && alice30) {
-  // Promise chain  
-  alice10.animate(aliceTumbling1, aliceTiming1).finished  
-    .then(() => {
-        return alice20
-                .animate(aliceTumbling1, aliceTiming1)
-                .finished;     
-    })
-    .then(() => {
-      return alice30
-              .animate(aliceTumbling1, aliceTiming1)
-              .finished;
-    })
-    .catch((err) => alert(`Error when promising ... ${err.message}`));
+
+async function animateImages(): Promise<void> {
+
+    if (alice10 && alice20 && alice30) {
+
+        const alice10Animation: Animation = await alice10.animate(aliceTumbling1, aliceTiming1).finished
+        const alice20Animation: Animation = await alice20.animate(aliceTumbling1, aliceTiming1).finished
+        const alice30Animation: Animation = await alice30.animate(aliceTumbling1, aliceTiming1).finished
+
+        const animations = await Promise.all([alice10Animation, alice20Animation, alice30Animation])
+
+        animations.forEach(animation => {
+            animation.finish()
+        })
+    }
+
+
 }
-else{
-  console.warn("#alice not found");
+
+async function runAnimation(): Promise<void> {
+    await animateImages()
 }
+
+runAnimation().then(r => {
+
+})
+
+
+// if(alice10 && alice20 && alice30) {
+//   // Promise chain
+//   alice10.animate(aliceTumbling1, aliceTiming1).finished
+//     .then(() => {
+//         return alice20
+//                 .animate(aliceTumbling1, aliceTiming1)
+//                 .finished;
+//     })
+//     .then(() => {
+//       return alice30
+//               .animate(aliceTumbling1, aliceTiming1)
+//               .finished;
+//     })
+//     .catch((err) => alert(`Error when promising ... ${err.message}`));
+// }
+// else{
+//   console.warn("#alice not found");
+// }
 
 // alice10
 //     .animate(aliceTumbling1, aliceTiming1)
