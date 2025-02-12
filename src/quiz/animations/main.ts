4a1bfc8ee1f8fb8e73468c9f4dab4708051ede5c
Henry Rothenberg <hsr205@aol.com>
2025-02-12 09:39:01 -0500

    Remove commented code

--- src/quiz/animations/main.ts
+++ src/quiz/animations/main.ts
@@ -42,37 +42,4 @@ runAnimation().then(() => {
     console.error(error);
 })
 
-
-// if(alice10 && alice20 && alice30) {
-//   // Promise chain
-//   alice10.animate(aliceTumbling1, aliceTiming1).finished
-//     .then(() => {
-//         return alice20
-//                 .animate(aliceTumbling1, aliceTiming1)
-//                 .finished;
-//     })
-//     .then(() => {
-//       return alice30
-//               .animate(aliceTumbling1, aliceTiming1)
-//               .finished;
-//     })
-//     .catch((err) => alert(`Error when promising ... ${err.message}`));
-// }
-// else{
-//   console.warn("#alice not found");
-// }
-
-// alice10
-//     .animate(aliceTumbling1, aliceTiming1)
-//     .finished
-//     .then((res) => {
-//         console.log(res);
-//         alice20
-//             .animate(aliceTumbling1, aliceTiming1)
-//             .finished
-//             .then((res) => {
-//                 console.log(res);
-//                 alice30.animate(aliceTumbling1, aliceTiming1);
-//             })
-//     });
   
