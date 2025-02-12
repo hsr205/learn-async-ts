252f792566d4169e49f8228cd6da390e51112f9a
Henry Rothenberg <hsr205@aol.com>
2025-02-12 07:33:05 -0500

    Add comment explanation

--- src/quiz/promise-2darray.ts
+++ src/quiz/promise-2darray.ts
@@ -1,3 +1,20 @@
+/*
+(1) Is there a difference in the flow of execution from when the code was in the setTimeout function?
+
+- Yes, without the `setTimeout()` function no sum is calculated in the `sum2DArray()` function. This results in
+`sum2DArray()` function exclusively containing print statements a opposed to the arithmetic operations
+conducted in the `setTimeout()` function.
+
+(2) Is the flow of execution in both these examples different from the flow of execution with the examples
+using the fetch() API, which also returns a promise?
+
+(3) If yes, explain the code execution difference. Add your explanation to src/quiz/promise-2darray.txt.
+
+- Yes. A function that leverages a Promise is not always an asynchronous operation. When leveraging the key-words
+`async` / `await` and the `fetch()` method as displayed in the fetch-products-async-await.ts module the method
+encapsulating these elements will always be an asynchronous operation.
+ */
+
 function sum2DArray(arr: number[][]): Promise<number> {
     return new Promise((resolve, reject) => {
         console.log('Sum called ... ');
