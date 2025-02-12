e93cb649cc5bae5f9dfd2588360d5b2b642329d8
Henry Rothenberg <hsr205@aol.com>
2025-02-12 09:03:39 -0500

    Simplify logic

--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -1,5 +1,4 @@
 function promiseSum2DArray(arr: number[][], rowIndex: number): Promise<number> {
-
     return new Promise((resolve, reject) => {
         console.log('Sum called ... ');
 
@@ -9,24 +8,18 @@ function promiseSum2DArray(arr: number[][], rowIndex: number): Promise<number> {
             reject('Cannot sum an empty array');
         }
 
-        if (rowIndex > arrLength - 1) {
+        if (rowIndex > arrLength - 1 || rowIndex < 0) {
             reject(`Index passed ${rowIndex} is invalid`);
         }
 
         setTimeout(() => {
-            let sum = 0;
-            let rowCount = 0;
-
-            arr.forEach((row) => {
-                if (rowCount === rowIndex) {
-                    sum = row.reduce((a, b) => a + b, 0);
-                }
-                rowCount++;
-            })
+            let sum: number = 0;
+            sum = arr[rowIndex].reduce((sum, item) => sum + item, 0);
             resolve(sum);
         }, 0);
         console.log('returning from sum');
     });
+
 }
 
 
@@ -36,23 +29,17 @@ const array2D_1 = [
     [7, 8, 9]
 ];
 
-const sumPromise2DArray = promiseSum2DArray(array2D_1, 0).then((value1: number): void => {
 
-    promiseSum2DArray(array2D_1, 1).then((value2: number) => {
+async function sumPromise2DArray(): Promise<number> {
 
-        promiseSum2DArray(array2D_1, 2).then((value3: number) => {
+    let values = await Promise.all([promiseSum2DArray(array2D_1, 0), promiseSum2DArray(array2D_1, 1), promiseSum2DArray(array2D_1, 2)])
 
-            const total = value1 + value2 + value3;
-            console.log(`sum2DArray: ${total}`);
+    return values.reduce((sum, item) => sum + item, 0);
 
-        }).catch((err) => {
-            console.log(err);
-        })
+}
 
-    }).catch((err) => {
-        console.log(err);
-    })
-
-}).catch((err) => {
-    console.log(err);
+sumPromise2DArray().then((value: number) => {
+    console.log(`Sum of 2D Array = ${value}`)
+}).catch((error) => {
+    console.error(error);
 });
